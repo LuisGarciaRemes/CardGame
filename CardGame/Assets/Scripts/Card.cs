b2d2f46414d54f021445fd2125c0284e4aee6d26
Luis Garcia Remes <luisagarciaremes@hotmail.com>
2020-06-06 19:08:39 -0600

    Created card scriptable object

---
+++ CardGame/Assets/Scripts/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+   [CreateAssetMenu(menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public string cardName;
+    public Sprite art;
+    public string cardText;
+    public string cardType;
+    public int cardCost;
+    public int cardValue;
+}
